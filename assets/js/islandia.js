4cae4c87a723b73ac2a80fd3cc3fb89c80f657b8
Anderson Araujo <161385114+andeAraujo@users.noreply.github.com>
2024-11-30 18:13:32 -0400

    formulario adicionado
    - Adicionado um formulário de feedback.

--- assets/js/islandia.js
+++ assets/js/islandia.js
@@ -27,3 +27,31 @@ document.querySelectorAll(".gallery-item").forEach(item => {
 document.querySelector(".modal .close").addEventListener("click", () => {
     document.getElementById("image-modal").style.display = "none"
 })
+
+document.getElementById("feedback-form").addEventListener("submit", function(event) {
+    event.preventDefault() // IMpede o envio padrao do formulario
+    
+    const name = document.getElementById("name").value.trim()
+    const email = document.getElementById("email").value.trim()
+    const message = document.getElementById("message").value.trim()
+
+    // valindando o email...
+    if (name === "" || email === "" || message === "") {
+        alert("Por favor, preencha todos os campos.")
+        return
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+        alert("Por favor, insira um e-mail válido.")
+        return
+    }
+
+    alert("Feedback enviado com sucesso! Obrigado por compartilhar sua opinião.")
+})
+
+function scrollToMap(mapId) {
+    const mapSection = document.getElementById(mapId);
+    if (mapSection) {
+        mapSection.scrollIntoView({ behavior: "smooth" });
+    }
+}
